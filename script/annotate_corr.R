9fbcf03c4817bf9db615d10c83afa98830f0e2ea
Aitor Gonzalez <gonzalez@jolitorax>
2017-01-10 15:18:25 +0100

    ENH Tested with some phenotype with quit good performances

--- script/annotate_corr.R
+++ script/annotate_corr.R
@@ -17,13 +17,18 @@ library(data.table)
 #outdir=Sys.getenv(c("OUTDIR"))
 
 ldlocus=fread(index2tag2corr_r2_tsv, sep="\t")
-ldlocus = unique(ldlocus[,c("index", "corr", "index2corr_r2"), with=FALSE])
 
-annotation=fread(annotation_tsv, sep="\t", header=FALSE)
-names(annotation) = c("rsid", "annotation")
+if (dim(ldlocus)[1] > 0) {
+    ldlocus = unique(ldlocus[,c("index", "corr", "index2corr_r2"), with=FALSE])
 
-index2annot_r2 = unique(merge(ldlocus, annotation, all=FALSE, by.x=c('corr'), by.y=c('rsid'), allow.cartesian=TRUE))
-index2annot_r2 = index2annot_r2[, c("index", "index2corr_r2", "annotation"), with=FALSE]
-write.table(index2annot_r2, index2annot_r2_tsv, quote=FALSE, sep="\t", row.names=FALSE, col.names=FALSE)
-#save(index2annot_r2, file=file.path(outdir, "index2annot_r2.Rda"))
+    annotation=fread(annotation_tsv, sep="\t", header=FALSE)
+    names(annotation) = c("rsid", "annotation")
+
+    index2annot_r2 = unique(merge(ldlocus, annotation, all=FALSE, by.x=c('corr'), by.y=c('rsid'), allow.cartesian=TRUE))
+    index2annot_r2 = index2annot_r2[, c("index", "index2corr_r2", "annotation"), with=FALSE]
+    write.table(index2annot_r2, index2annot_r2_tsv, quote=FALSE, sep="\t", row.names=FALSE, col.names=FALSE)
+    #save(index2annot_r2, file=file.path(outdir, "index2annot_r2.Rda"))
+} else {
+file.create(index2annot_r2_tsv)
+}
 
